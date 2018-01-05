af16200743d97eb4520e0e9c9e4b88ac60aa5910
がお <gaotuki@gmail.com>
2018-01-05 21:36:46 +0900

    ティザーサイトの実装

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,79 +1,44 @@
 <template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two"></div>
-    <div class="Triangle Triangle--one"></div>
-    <div class="Triangle Triangle--three"></div>
-    <div class="Triangle Triangle--four"></div>
+  <div class="logo" :class="{ show: isShow }">
+    <p>2018年秋<br>政剣マニフェスティア<br>
+非公式 オンリーイベント<br>
+東京近郊某所にて 開催決定！</p>
   </div>
 </template>
 
-<style>
-.VueToNuxtLogo {
-  display: inline-block;
-  animation: turn 2s linear forwards 1s;
-  transform: rotateX(180deg);
-  position: relative;
-  overflow: hidden;
-  height: 180px;
-  width: 245px;
-}
-
-.Triangle {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-}
-
-.Triangle--one {
-  border-left: 105px solid transparent;
-  border-right: 105px solid transparent;
-  border-bottom: 180px solid #41B883;
-}
-
-.Triangle--two {
-  top: 30px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 87.5px solid transparent;
-  border-right: 87.5px solid transparent;
-  border-bottom: 150px solid #3B8070;
-}
-
-.Triangle--three {
-  top: 60px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 70px solid transparent;
-  border-right: 70px solid transparent;
-  border-bottom: 120px solid #35495E;
-}
-
-.Triangle--four {
-  top: 120px;
-  left: 70px;
-  animation: godown 0.5s linear forwards 3s;
-  border-left: 35px solid transparent;
-  border-right: 35px solid transparent;
-  border-bottom: 60px solid #fff;
-}
-
-@keyframes turn {
-  100% {
-    transform: rotateX(0deg);
+<script>
+export default {
+  data () {
+    return { isShow: false }
+  },
+  mounted () {
+    this.isShow = true
   }
 }
+</script>
 
-@keyframes godown {
-  100% {
-    top: 180px;
-  }
+<style scoped>
+@font-face {
+  font-family: "Makinas-Scrap-5";
+  src: url("/assets/font/Makinas-Scrap-5.woff") format("truetype");
 }
 
-@keyframes goright {
-  100% {
-    left: 70px;
+.logo{
+  text-align: center;
+  font-size : 7.5vw;
+  font-family: "Makinas-Scrap-5";
+  line-height: 1.25;
+  opacity: 0;
+}
+
+.logo.show {
+  transition: 1s opacity ease;
+  opacity: 1;
+}
+
+@media screen and (orientation: landscape) {
+  .logo {
+    font-size: 2.5rem;
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,7 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        seimani-only-2018
-      </h1>
-      <h2 class="subtitle">
-        政剣マニフェスティアオンリーイベント2018公式サイトです
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+    <div class="logoContainer">
+      <logo class="logo" />
     </div>
   </section>
 </template>
@@ -26,33 +16,31 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .container {
-  min-height: 100vh;
+  width: 100vw;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-image: url("/assets/img/teaser_bg.jpg");
+  background-position: center center;
+  background-size: cover;
+}
+
+.logoContainer {
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
-  align-items: center;
-  text-align: center;
+  align-items: flex-end;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.logo {
+  margin-bottom: 3rem;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media screen and (orientation: landscape) {
+  .container {
+    background-size: contain;
+  }
 }
 </style>
