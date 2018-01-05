09fe9cd99908ad98dbc0eb7a1e96fdb794c09d45
がお <gaotuki@gmail.com>
2018-01-05 22:31:45 +0900

    ヘッダ情報の追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,31 @@
+const shared = {
+  title: '政剣マニフェスティアオンリーイベント2018 公式サイト',
+  description: '政剣マニフェスティアオンリーイベント2018の公式サイトです',
+  image: '/ogp_icon.jpg'
+}
+
 module.exports = {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'seimani-only-2018',
+    title: shared.title,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '政剣マニフェスティアオンリーイベント2018公式サイトです' }
+      { name: 'google', content: 'notranslate' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:locale', content: 'ja_JP' },
+      { property: 'og:title', content: shared.title },
+      { property: 'og:site_name', content: shared.title },
+      { property: 'og:url', content: 'https://festia.moe' },
+      { property: 'og:description', content: shared.description },
+      { property: 'og:image', content: shared.image },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:title', content: shared.title },
+      { name: 'twitter:image', content: shared.image },
+      { name: 'twitter:description', content: shared.description },
+      { hid: 'description', name: 'description', content: shared.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
